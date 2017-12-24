bd00e30910275bc11e5c052c698c61affe2bb4e0
Ankur Rai <rai.ankur0007@gmail.com>
2017-12-24 12:01:14 +0530

    tic_tac_toe player generate

---
+++ player.js
@@ -0,0 +1,23 @@
+let Player = function(name, symbol) {
+  this.name = name;
+  this.symbol = symbol;
+  this.playerMoves = [];
+};
+
+Player.prototype.getPlayerMoves = function() {
+  return this.playerMoves
+};
+
+Player.prototype.storeMove = function(move) {
+  this.playerMoves.push(move);
+};
+
+Player.prototype.getSymbol = function(move) {
+  return this.symbol;
+};
+
+Player.prototype.getName = function() {
+  return this.name;
+}
+
+module.exports = Player;
